5c860878e16bdc99b2fe636dc6d91fe8b4525585
Lyhengsun <hengsun2.168@gmail.com>
2024-01-24 19:26:29 +0700

    added data for current user in the data folder

---
+++ lib/data/data.dart
@@ -0,0 +1,8 @@
+import 'package:flutter_facebook_clone/models/models.dart';
+
+final User currentUser = User(
+    name: "Aijou Rentarou",
+    imageURL:
+        "https://static.wikia.nocookie.net/100kanojo/images/e/e2/RentarouAnimeInfobox.png/revision/latest?cb=20240118045820");
+
+
